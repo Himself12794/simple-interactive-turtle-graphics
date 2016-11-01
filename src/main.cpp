3687d0adcfc1c4f975c9ec8bc21ad37d4d782495
phwhitin <phwhitin@cisco.com>
2016-11-01 08:56:12 -0400

    added .gitignore, initial code commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Nov 1, 2016
+ *      Author: phwhitin
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+	cout << "Test" << endl;
+}
+
