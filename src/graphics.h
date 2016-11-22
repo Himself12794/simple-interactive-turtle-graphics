fb7bd21f9179dfa5c429756ca4adaf146c1ec5f7
phwhitin <phwhitin@cisco.com>
2016-11-21 19:04:20 -0500

    added comments

--- src/graphics.h
+++ src/graphics.h
@@ -8,6 +8,15 @@
 #ifndef GRAPHICS_H_
 #define GRAPHICS_H_
 
+/**
+ * The name space for all important graphics items.
+ * TODO return multi-threading capabilities to the window class
+ * TODO allow multiple windows
+ * TODO reorganize the window class so OpenGL interaction is done with
+ * non-instance methods
+ * TODO add the option for the turtle to draw a circle
+ * TODO allow the turtle to not go out of bounds of the window
+ */
 namespace t_graphics {
 
 	class turtle;
